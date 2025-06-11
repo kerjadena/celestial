cf19e9134aa281922cbc50106f3df2dbb5e854fe
Adenawati Hasim <71482@fif.co.id>
2025-06-11 12:37:15 +0700

    update boolean

--- exercise/day  1/Hello World/src/Main.java
+++ exercise/day  1/Hello World/src/Main.java
@@ -2,24 +2,39 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        long tipelong = 1234567890;
-        int tipeint = 1111;
-        double tipedouble = 22.22;
-        float tipefloat = 3.3333f;
-        char tipechar = 'A';
-        boolean isDenaYeppo =  true;
-        String nama = "Adenadenadena";
-        int[] angka = {4,4,4,4,4};
+//        long tipelong = 1234567890;
+//        int tipeint = 1111;
+//        double tipedouble = 22.22;
+//        float tipefloat = 3.3333f;
+//        char tipechar = 'A';
+//        boolean isDenaYeppo =  true;
+//        String nama = "Adenadenadena";
+//        int[] angka = {4,4,4,4,4};
+//
+//        System.out.println("Hello, World!");
+//        System.out.println("Long : " + tipelong);
+//        System.out.println("Integer : " + tipeint );
+//        System.out.println("Double : " + tipedouble );
+//        System.out.println("Float : " +tipefloat);
+//        System.out.println("Char : "+tipechar );
+//        System.out.println("Boolean : " +isDenaYeppo);
+//        System.out.println("Nama : "+ nama );
+//        System.out.println("Array : " +java.util.Arrays.toString(angka) ) ;
 
-        System.out.println("Hello, World!");
-        System.out.println("Long : " + tipelong);
-        System.out.println("Integer : " + tipeint );
-        System.out.println("Double : " + tipedouble );
-        System.out.println("Float : " +tipefloat);
-        System.out.println("Char : "+tipechar );
-        System.out.println("Boolean : " +isDenaYeppo);
-        System.out.println("Nama : "+ nama );
-        System.out.println("Array : " +java.util.Arrays.toString(angka) ) ;
+        int tambah = 11+5;
+        long kali = 78*99;
+
+        boolean kurangdari = (10<7);
+        boolean besardari = (99>3);
+
+        boolean result = (8>7) && (4<99);
+
+        System.out.println("Hasil tambah ="+ tambah);
+        System.out.println("Hasil kali ="+ kali);
+        System.out.println("10<7 adalah "+ kurangdari);
+        System.out.println("99>3 adalah "+ besardari);
+        System.out.println("Perbandingannya "+ result);
+
 
 
         }
