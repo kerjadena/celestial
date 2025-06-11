edcede142834e106074885d88565ac5960344666
Adenawati Hasim <71482@fif.co.id>
2025-06-11 11:52:11 +0700

    update tipe data

--- exercise/day  1/Hello World/src/Main.java
+++ exercise/day  1/Hello World/src/Main.java
@@ -2,7 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        long tipelong = 1234567890;
+        int tipeint = 1111;
+        double tipedouble = 22.22;
+        float tipefloat = 3.3333f;
+        char tipechar = 'A';
+        boolean isDenaYeppo =  true;
+        String nama = "Adenadenadena";
+        int[] angka = {4,4,4,4,4};
+
         System.out.println("Hello, World!");
+        System.out.println("Long : " + tipelong);
+        System.out.println("Integer : " + tipeint );
+        System.out.println("Double : " + tipedouble );
+        System.out.println("Float : " +tipefloat);
+        System.out.println("Char : "+tipechar );
+        System.out.println("Boolean : " +isDenaYeppo);
+        System.out.println("Nama : "+ nama );
+        System.out.println("Array : " +java.util.Arrays.toString(angka) ) ;
+
+
         }
 
 }
