b95d30119c81d163d20c72f715057dff5baab685
Adenawati Hasim <71482@fif.co.id>
2025-06-11 14:38:50 +0700

    practical exam

--- exercise/day  1/Control Flow/src/Main.java
+++ exercise/day  1/Control Flow/src/Main.java
@@ -2,54 +2,68 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = -8 ;
-        if (a > 0) {
-            System.out.println("Bilangan bilangan positif");
-        }else if (a<0) {
-            System.out.println("Bilangan bilangan negatif");
-        }else { System.out.println("Ini adalah angka 0");
-        }
 
-        int hari = 9;
-        switch (hari) {
-            case 1 :
-                System.out.println("Hari senin");
-                break;
-            case 2 :
-                System.out.println("Hari selasa");
-                break;
-            case 3 :
-                System.out.println("Hari rabu");
-                break;
-            case 4 :
-                System.out.println("Hari kamis");
-                break;
-            case 5 :
-                System.out.println("Hari jumat");
-                break;
-            case 6 :
-                System.out.println("Hari sabtu");
-                break;
-            case 7 :
-                System.out.println("Hari minggu");
-                break;
-            default:
-                System.out.println("Masukkan angka 1-7");
+      //  print even numbers from 1 to 20
+//        for (int i = 1; i<=20; i++){
+//            if (i%2 ==0){
+//                System.out.println(i);
+//            }
+      // calculate factorial of a given number
+        int number = 8;
+        int factorial  = 1;
 
-                // while loop
+        for (int i=number; i>0;i--){
+            factorial *= i;
+        } System.out.println(("Faktorial dari 8 adalah "+ factorial));
 
-                int i = 0;
-                while (i < 5) {
-                    System.out.println("Perulangan ke-" + i);
-                    i++;
-                }
-
-                // do while loop
-                int j = 0;
-                do {
-                    System.out.println("Perulangan ke-" + j);
-                    j++;
-                } while (j < 5);
+        // IF ELSE & SWITCH
+//        int a = -8 ;
+//        if (a > 0) {
+//            System.out.println("Bilangan bilangan positif");
+//        }else if (a<0) {
+//            System.out.println("Bilangan bilangan negatif");
+//        }else { System.out.println("Ini adalah angka 0");
+//        }
+//
+//        int hari = 9;
+//        switch (hari) {
+//            case 1 :
+//                System.out.println("Hari senin");
+//                break;
+//            case 2 :
+//                System.out.println("Hari selasa");
+//                break;
+//            case 3 :
+//                System.out.println("Hari rabu");
+//                break;
+//            case 4 :
+//                System.out.println("Hari kamis");
+//                break;
+//            case 5 :
+//                System.out.println("Hari jumat");
+//                break;
+//            case 6 :
+//                System.out.println("Hari sabtu");
+//                break;
+//            case 7 :
+//                System.out.println("Hari minggu");
+//                break;
+//            default:
+//                System.out.println("Masukkan angka 1-7");
+//
+//                // while loop
+//
+//                int i = 0;
+//                while (i < 5) {
+//                    System.out.println("Perulangan ke-" + i);
+//                    i++;
+//                }
+//
+//                // do while loop
+//                int j = 0;
+//                do {
+//                    System.out.println("Perulangan ke-" + j);
+//                    j++;
+//                } while (j < 5);
         }
     }
-}
