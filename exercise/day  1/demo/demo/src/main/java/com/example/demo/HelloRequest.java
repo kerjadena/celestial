27f29f219180db426a303c56a5ccc0a6a3655021
Adenawati Hasim <71482@fif.co.id>
2025-06-11 17:44:14 +0700

    demo,ppt,encapsulation

---
+++ exercise/day  1/demo/demo/src/main/java/com/example/demo/HelloRequest.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+public class HelloRequest {
+    private String name;
+    public HelloRequest(){}
+    public HelloRequest(String name){this.name = name;}
+    public String getName(){return name;}
+    public void setName(String name){this.name = name;}
+}
